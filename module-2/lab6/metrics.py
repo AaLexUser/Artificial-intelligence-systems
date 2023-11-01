f65eefa906102db53ec005c471b8dd66ebc2c642
Alexey Lapin <333893@niuitmo.ru>
2023-11-01 12:47:44 +0300

    small updates

--- module-2/lab6/metrics.py
+++ module-2/lab6/metrics.py
@@ -12,22 +12,22 @@ def confusion_matrix_prob(pred_probs, true_y, threshold):
 
     return res
 
-
+# TP + TN / TP + TN + FP + FN
 def accuracy(conf):
     return (conf[1][1] + conf[0][0]) / (conf[1][1] + conf[0][0] + conf[1][0] + conf[0][1])
 
-
+# TP / TP + FN
 def recall(conf):
     return conf[1][1] / (conf[1][1] + conf[0][1])
 
-
+# TP / TP + FP
 def precision(conf):
     return conf[1][1] / (conf[1][1] + conf[1][0])
 
-
+# recall
 def tpr(conf):
     return recall(conf)
 
-
+# FP / FP + TN
 def fpr(conf):
     return conf[1][0] / (conf[1][0] + conf[0][0])
